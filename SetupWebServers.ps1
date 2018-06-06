4dcd02f79bf760cb32baa337d1508cd3a4267727
Robert Kuehfus <rokuehfu@microsoft.com>
2018-06-06 15:52:39 -0400

    Added .Net Core 2.1.0 Install

--- SetupWebServers.ps1
+++ SetupWebServers.ps1
@@ -4,9 +4,6 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 # Install ASP.NET 4.6
 Install-WindowsFeature Web-Asp-Net45
 
-# Delete contents of wwwroot
-Remove-Item -Recurse C:\inetpub\wwwroot\*
-
 # Install Web Management Service (enable and start service)
 Install-WindowsFeature -Name Web-Mgmt-Service
 Set-ItemProperty -Path  HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
@@ -28,3 +25,13 @@ $arguments= '/i ' + $msiFile + ' ADDLOCAL=ALL /qn /norestart LicenseAccepted="0"
 $proc = (Start-Process -file msiexec -arg $arguments -Passthru)
 $proc | Wait-Process
 Get-Content $logFile
+
+# Install Microsoft .Net Core 2.1.0
+# Download file from Microsoft Downloads and save to local temp file (%LocalAppData%/Temp/2)
+$exeFileNetCore = [System.IO.Path]::GetTempFileName() | Rename-Item -NewName "dotnet-hosting-2.1.0-win.exe" -PassThru
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/9/1/7/917308D9-6C92-4DA5-B4B1-B4A19451E2D2/dotnet-hosting-2.1.0-win.exe" -OutFile $exeFileNetCore
+# Run the exe with arguments
+$proc = (Start-Process -FilePath $exeFileNetCore.Name.ToString() -ArgumentList ('/install','/quiet') -WorkingDirectory $exeFileNetCore.Directory.ToString() -Passthru)
+$proc | Wait-Process
+#Restart iis
+Start-Process -FilePath C:\Windows\System32\iisreset.exe -ArgumentList /RESTART 
