64f3187f1befb12ce00b06e259d3681a7f0c0d6f
Robert Kuehfus <rkuehfus@live.com>
2018-11-25 10:55:48 -0500

    added Azure monitor sink to scaleset and some notes in the PS deploy script

--- Student/Resources/DeployMonHackEnv.ps1
+++ Student/Resources/DeployMonHackEnv.ps1
@@ -3,12 +3,15 @@
 #Tip: Show the Integrated Terminal from View\Integrated Terminal
 #Tip: click on a line and press "F8" to run the line of code
 
+#Make sure you are running the latest version of the Azure PowerShell modules, uncomment the line below and run it (F8)
+# Install-Module -Name AzureRM -Force -Scope CurrentUser -AllowClobber
+
 #Step 1: Use a name no longer then five charactors all lowercase.  Your initials would work well if working in the same sub as others.
 $MonitoringHackName = 'your-initials-here'
 
 #Step 2: Create ResourceGroup after updating the location to one of your choice. Use get-AzureRmLocation to see a list
 Connect-AzureRmAccount
-New-AzureRMResourceGroup -Name $MonitoringHackName -Location 'East US'
+New-AzureRMResourceGroup -Name $MonitoringHackName -Location 'East US' #The hack uses a few features in preview so its best to leave this in East US
 $rg = get-AzureRmresourcegroup -Name $MonitoringHackName
 
 #Step 3: Create Key Vault and set flag to enable for template deployment with ARM
@@ -22,6 +25,5 @@ Set-AzureKeyVaultSecret -VaultName $MonitoringHackVaultName -Name "VMPassword" -
 (Get-AzureRmKeyVault -VaultName $MonitoringHackVaultName).ResourceId
 
 #Step 6: Run deployment below after updating and SAVING the parameter file with your key vault info.  Make sure to update the paths to the json files or run the command from the same directory
-#Note: This will deploy VMs using B series VMs.  By default a subscription is limited to 10 cores of B Series per region.  You may have to request more cores or
-# choice another region if you run into quota errors on your deployment.  Also feel free to modify the ARM template to use a different VM Series.
+#Note: You may want to adjust the VM series deployed in the ARM template. Feel free to modify the ARM template to use a different VM Series.
 New-AzureRmResourceGroupDeployment -Name $MonitoringHackName -ResourceGroupName $MonitoringHackName -TemplateFile '.\VMSSazuredeploy.json' -TemplateParameterFile '.\azuredeploy.parameters.json'
